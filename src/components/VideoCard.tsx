c926818e70d700901428e00140bfd27607d35ab8
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-08-30 22:18:00 +0530

    add props to VideCard component

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,19 +1,19 @@
-export default function VideoCard() {
+export default function VideoCard(props:any) {
     return <div>
-        <img src="/thumbnail1.jpg" className="rounded-xl"></img>
+        <img src={props.image} className="rounded-xl"></img>
         <div className="grid grid-cols-12 pt-2">
             <div className="col-span-1">
-                <img className={"rounded-full w-20 h-20"} src="/thumbnail1.jpg"></img>
+                <img className={"rounded-full w-20 h-20"} src={props.thumbImage}></img>
             </div>
             <div className="col-span-11">
                 <div>
-                    Jawan: Chaleya(Hindi) | Sharukh Khan
+                    {props.title}
                 </div>
                 <div className="col-span-11 text-gray-400 text-base">
-                    T-Series
+                    {props.author}
                 </div>
                 <div className="col-span-11 text-gray-400 text-base">
-                    64Mn | 13 days ago
+                    {props.views} | {props.timestamp}
                 </div>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <VideoCard />
+      <VideoCard 
+        title={"How to learn coding in 30 days | 30 day plan | Code with me"}
+        image={"/photo.jpg"}
+        thumbImage={"/photo.jpg"}
+        author={"Harkirat Singh"}
+        views={"100k"}
+        timestamp={"2 days"} />
     </div>
   )
 }
